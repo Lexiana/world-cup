56d069f7eccf228e3fbba1cc0e167cb757c96e21
Lexiana <schaller.lexi@gmail.com>
2024-07-27 17:30:05 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,53 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# Clean tables
+echo $($PSQL "TRUNCATE teams, games")
+
+# Add each unique team to the teams table
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    TEAM_ID_WINNER=$($PSQL "select team_id from teams where name='$WINNER'")
+    if [[ -z $TEAM_ID_WINNER ]]
+    then
+      INSERT_TEAMWINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERT_TEAMWINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $WINNER
+      fi
+    fi
+    TEAM_ID_OPPONENT=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    if [[ -z $TEAM_ID_OPPONENT ]]
+    then
+      INSERT_TEAMOP_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_TEAMOP_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into teams, $OPPONENT
+      fi
+    fi
+  fi
+done
+
+# Add data into games table
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    GAME_ID=$($PSQL "select game_id from games where year=$YEAR and round='$ROUND' and winner_id=$WINNER_ID and opponent_id=$OPPONENT_ID")
+
+    if [[ -z $GAME_ID ]]
+    then
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into games, $YEAR $ROUND : $WINNER vs $OPPONENT
+      fi
+    fi
+
+
+  fi
+done
